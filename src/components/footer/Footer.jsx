23599870f7d7e0d6b98b79ddd1b838941be36e43
OritLeshem <azmono@gmail.com>
2023-02-05 20:03:23 -0500

    footer links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => {
       <a href="#" className='footer__logo'>Orit Leshem  </a>
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
-        <li><a href="experience">Experience</a></li>
-        <li><a href="portfolio">Portfolio</a></li>
-        <li><a href="contact">Contact</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
       <div className="footer__socials">
         <a href="https://www.linkedin.com/in/orit-leshem/"><BsLinkedin /></a>
