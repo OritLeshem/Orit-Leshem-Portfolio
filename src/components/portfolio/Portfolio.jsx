e45f2f04abdc45324cb758979c5eeecb54ea5f7c
OritLeshem <azmono@gmail.com>
2023-04-20 11:41:06 -0400

    chatGPT

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Portfolio.css'
 import FIVERR from '../../assets/fiverr.svg'
+import SPOTIFY from '../../assets/spotify1.svg'
 import NOTE from '../../assets/note1.svg'
 import MEME from '../../assets/meme.svg'
-import CONTACT from '../../assets/contacts.svg'
+// import CONTACT from '../../assets/contacts.svg'
+import CONTACT from '../../assets/bitcoin2.svg'
+
+import CHATGPT from '../../assets/chatgpt3.svg'
+
 
 const Portfolio = () => {
   return (
@@ -13,13 +18,41 @@ const Portfolio = () => {
       <div className="container portfolio__container">
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
+            <img src={SPOTIFY} alt="portfolio" />
+          </div>
+          <h3>Inspired by Spotify </h3>
+          <div className="portfolio__item-desc-container">
+            <h3 className="portfolio__item-desc" >React | Redux | SCSS | Node.js | MongoDB | REST API</h3>
+          </div>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com/OritLeshem/spotify-app" className='btn' target='_blank'>Github</a>
+            <a href="https://spotify-app-uejg.onrender.com/" className='btn btn-primary' target='_blank'>Live-demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
             <img src={FIVERR} alt="portfolio" />
           </div>
-          <h3>finderr - inspired by fiverr</h3>
-          <h3>react.js | scss | node.js | mongoDB</h3>
+          <h3>Inspired by Fiverr</h3>
+          <div className="portfolio__item-desc-container">
+            <h3 className="portfolio__item-desc">React | Redux | SCSS | Node.js | MongoDB</h3>
+          </div>
           <div className='portfolio__item-cta'>
             <a href="https://github.com/OritLeshem/sprint4-fiverr" className='btn' target='_blank'>Github</a>
             <a href="https://finderr.onrender.com" className='btn btn-primary' target='_blank'>Live-demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={CHATGPT} alt="portfolio" />
+          </div>
+          <h3>Inspired by chatGPT</h3>
+          <div className="portfolio__item-desc-container">
+            <h3 className="portfolio__item-desc">React | CSS | Node.js | REST API</h3>
+          </div>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com/OritLeshem/chat-gpt" className='btn' target='_blank'>Github</a>
+            <a href="https://chat-gpt-az9j.onrender.com" className='btn btn-primary' target='_blank'>Live-demo</a>
           </div>
         </article>
 
@@ -27,20 +60,24 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={CONTACT} alt="portfolio" />
           </div>
-          <h3>Mr Bitcoin -contact list with Bitcoin api</h3>
-          <h3>vue.js | scss</h3>
+          <h3>Contact list with info about Bitcoin rate</h3>
+          <div className="portfolio__item-desc-container">
+            <h3 className="portfolio__item-desc">Vue.js | SCSS | REST API</h3>
+          </div>
           <div className='portfolio__item-cta'>
             <a href="https://github.com/OritLeshem/Mr-Bitcoin" className='btn' target='_blank'>Github</a>
             <a href="https://oritleshem.github.io/Mr-Bitcoin/" className='btn btn-primary' target='_blank'>Live-demo</a>
           </div>
         </article>
 
-        <article className='portfolio__item'>
+        {/* <article className='portfolio__item'>
           <div className="portfolio__item-image">
             <img src={NOTE} alt="portfolio" />
           </div>
-          <h3>Appsus - inspired by google keep and gmail</h3>
-          <h3>react.js | css</h3>
+          <h3>Appsus - inspired by google keep</h3>
+          <div className="portfolio__item-desc-container">
+            <h3 className="portfolio__item-desc">React | CSS</h3>
+          </div>
           <div className='portfolio__item-cta'>
             <a href="https://github.com/OritLeshem/appsus" className='btn' target='_blank'>Github</a>
             <a href="https://oritleshem.github.io/appsus/" className='btn btn-primary' target='_blank'>Live-demo</a>
@@ -51,12 +88,14 @@ const Portfolio = () => {
             <img src={MEME} alt="portfolio" />
           </div>
           <h3>MIMI - meme generator</h3>
-          <h3>js| canvas | html |css</h3>
+          <div className="portfolio__item-desc-container">
+            <h3 className="portfolio__item-desc">JS | CANVAS | HTML | CSS</h3>
+          </div>
           <div className='portfolio__item-cta'>
             <a href="https://github.com/OritLeshem/Meme-Generator" className='btn' target='_blank'>Github</a>
             <a href="https://oritleshem.github.io/Meme-Generator/" className='btn btn-primary' target='_blank'>Live-demo</a>
           </div>
-        </article>
+        </article> */}
       </div>
     </section>
 
