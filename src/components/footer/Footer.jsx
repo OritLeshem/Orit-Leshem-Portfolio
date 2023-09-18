03a596a689387e72f165cbb75b14e8163a777ccc
OritLeshem <azmono@gmail.com>
2023-09-18 13:32:24 -0400

    qr

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,20 +3,25 @@ import './Footer.css'
 import { BsLinkedin } from 'react-icons/bs'
 import { FaGithub } from 'react-icons/fa'
 import { AiOutlinePhone } from 'react-icons/ai'
+import QR from '../../assets/qr-code.png';
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Orit Leshem  </a>
-      <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-      <div className="footer__socials">
-        <a href="https://www.linkedin.com/in/orit-leshem/"><BsLinkedin /></a>
-        <a href="https://github.com/OritLeshem"><FaGithub /></a>
+      <div>
+        <div className='qr'><img className='qr-img' src={QR} alt="" /></div>
+
+        <a href="#" className='footer__logo'>Orit Leshem  </a>
+        <ul className='permalinks'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <div className="footer__socials">
+          <a href="https://www.linkedin.com/in/orit-leshem/"><BsLinkedin /></a>
+          <a href="https://github.com/OritLeshem"><FaGithub /></a>
+        </div>
       </div>
 
     </footer>
